4dbdcc9318bfea07daf3b6f0021c0d84bd83c546
Marina Geller Yamaguti <111259549+marina-yamaguti@users.noreply.github.com>
2022-09-26 15:30:52 -0300

    Update Voo.java
    Exercício 4 - torna Voo uma classe abstrata

--- Voo.java
+++ Voo.java
@@ -1,42 +1,32 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class Voo implements Contavel{
+public abstract class Voo implements Contavel{
 	
 	public enum Status { CONFIRMADO, ATRASADO, CANCELADO };
 
 	private static int totalVoos;	
 	private LocalDateTime datahora;
-	private Duration duracao;
-	private Rota rota;
 	private Status status;
 	
-	public Voo(Rota rota, LocalDateTime datahora, Duration duracao) {
+	public Voo(LocalDateTime datahora) {
 		totalVoos++;
-		this.rota = rota;
 		this.datahora = datahora;
-		this.duracao = duracao;
 		this.status = Status.CONFIRMADO; // default é confirmado
 	}
 
-	public Voo(Rota rota, Duration duracao) { //exercicio 1
+	public Voo() { //exercicio 1
 		totalVoos++;
-		this.rota = rota;
-		this.duracao = duracao;
 		datahora = LocalDateTime.of(2016, 8, 12, 12, 00);
 		this.status = Status.CONFIRMADO; 
 	}
 	
-	public Rota getRota() {
-		return rota;
-	}
+	public abstract Rota getRota();
+
+	public abstract Duration getDuracao();
 	
 	public LocalDateTime getDatahora() {
 		return datahora;
-	}
-	
-	public Duration getDuracao() {
-		return duracao;
 	}
 	
 	public Status getStatus() {
@@ -56,6 +46,6 @@ public class Voo implements Contavel{
 	}
 
 	public String toString() {
-        return  getStatus() + " " + getDatahora() + "("+getDuracao()+"): " + getRota();
+        return  getStatus() + " " + getDatahora() + getRota();  // (duracao): rota;
     }
 }
