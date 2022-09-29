57658758a0a0145f9818a9a7983d31412fdf8dd6
Gabriel Quintana <106264426+ogabrielquintana@users.noreply.github.com>
2022-09-29 00:06:37 -0300

    Update VooDireto.java

--- VooDireto.java
+++ VooDireto.java
@@ -16,12 +16,18 @@ public class VooDireto extends Voo {
         return rota;
     }
 
+    @Override
 	public Duration getDuracao() {
-        return null;
+        Duration duration;
+        double d = Geo.distancia(rota.getOrigem().getLocal(), rota.getDestino().getLocal());
+        double e = ((805 / d) + 30);
+        long f = Double.valueOf(e).longValue();
+        duration = Duration.ofMinutes(f);
+        return duration;
     }
 	
 	public String toString() {
-        return  getStatus() + " " + getDatahora() + getRota();  // (duracao): rota;
+        return  getStatus() + " " + getDuracao() + " " + getDatahora() + getRota();  // (duracao): rota;
     }
     
 }
